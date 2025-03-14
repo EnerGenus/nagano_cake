68eeb50041b3eb57bf3ee2445c9d746b0d74a5be
kuropAqn <kuropan0819@gmail.com>
2025-03-14 06:25:03 +0000

    Add deviseCustomer/remove deviseUser

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :customers, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
