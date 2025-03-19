31d20d3464c4675ac01903fd1d6e85e0ff78a110
EC2 Default User <ec2-user@ip-172-31-1-93.ap-northeast-1.compute.internal>
2025-03-19 10:03:54 +0000

    Add address#index,create,edit,update,destroy Add view/address/_address_form

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -5,27 +5,36 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
+    @address = Address.find(params[:id])
+    unless @address.customer.id == current_customer.id
+      redirect_to addresses_path
+    end
   end
 
   def create
+    @addresses = Address.where(customer_id: current_customer.id)
     @address = current_customer.addresses.new(address_params)
     if @address.save
-      redirect_to request.referer, notice: '配送先を登録しました'
+      redirect_to request.referer
     else
-      @addresses = current_customer.addresses
-      @address = Address.new
-      flash.now[:alert] = "配送先の登録に失敗しました。" 
       render :index
     end
 
   end
 
   def update
-
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to request.referer
   end
 
   private
