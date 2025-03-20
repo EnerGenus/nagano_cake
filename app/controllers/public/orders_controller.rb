bbea872cf52b8e2622d87dd9d51925424b442487
EC2 Default User <ec2-user@ip-172-31-1-93.ap-northeast-1.compute.internal>
2025-03-20 09:29:59 +0000

    pub/orders#new <on the way>

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,14 +1,16 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+    @addresses = current_member.addresses.all  # 会員が登録した配送先一覧を取得
   end
 
   def confirm
   end
 
-  def done
+  def create
   end
 
-  def create
+  def done
   end
 
   def index
--- app/models/address.rb
+++ app/models/address.rb
@@ -5,4 +5,9 @@ class Address < ApplicationRecord
   validates :address, presence: true
   validates :name, presence: true
 
+  # view/pubric/orders/new.html.erbで使うメソッド
+  def full_address
+    "〒#{post_code} #{address} #{name}"
+  end
+
 end
