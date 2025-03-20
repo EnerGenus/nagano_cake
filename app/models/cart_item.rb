69ab0f1ac5bd0589b66968fdadf831ea2dda595a
kuropAqn <kuropan0819@gmail.com>
2025-03-20 09:04:27 +0000

    Add admin_customer_edit/index/show bug admin_order_detail

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,10 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
+  belongs_to :order
+
+  def subtotal
+    item.price_including_tax * quantity
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,29 @@
 Rails.application.routes.draw do
 
   namespace :admin do
+    root to: 'homes#top'
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:new, :index, :create, :show, :edit, :update]
-    root to: 'homes#top'
     resources :customers, only: [:show, :index, :edit, :update]
-    resources :orders, only: [:index]
+    resources :orders, only: [:index, :show, :update]
+    resources :order_details, only: [:update]
   end
-  
+
   scope module: :public do
     root 'homes#top'
-    resources :addresses, only: [:index, :create, :edit, :update, :destroy]
     get 'homes/about' => 'homes#about', as: 'about'
-    resources :items, only: [:index, :show]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
     get 'customers/mypage' => 'customers#show'
     get 'customers/information/edit' => 'customers#edit'
     patch 'customers/information' => 'customers#update'
     get  'customers/unsubscribe' => 'customers#unsubscribe', as:'unsubscribe' #確認画面へのパス
     patch 'customers/withdraw' => 'customers#withdraw', as:'withdraw' #退会処理用のアクションパス
+    get 'orders/done'
+    post 'orders/confirm'
+    resources :addresses, only: [:index, :create, :edit, :update, :destroy]
+    resources :items, only: [:index, :show]
+    resources :cart_items, only: [:index, :create, :update, :destroy]
+    resources :orders, only: [:new, :create, :index, :show]
   end
 
   devise_for :customers, controllers: {
