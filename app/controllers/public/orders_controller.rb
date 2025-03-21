7756347ed1cfa933c07aeb6191526fb7f50d28ef
EC2 Default User <ec2-user@ip-172-31-1-93.ap-northeast-1.compute.internal>
2025-03-21 05:21:30 +0000

    add pub/orders#confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,6 +5,20 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
+    if params[:order][:address_option] == "0" 
+      # address_option=0 つまり「お届け先」で「ご自身の住所」を選択している時
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.family_name + current_customer.first_name
+    elsif params[:order][:address_option] == "1" 
+      # address_option=0 つまり「お届け先」で「登録済住所から選択」を選択している時
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    end
+    # お届け先に「新しいお届け先」が選択されている場合は、view内の記述で格納済み
   end
 
   def create
@@ -19,5 +33,13 @@ class Public::OrdersController < ApplicationController
   def show
   end
 
-
+  private
+  def order_params
+    params.require(:order).permit(
+      :payment_method,
+      :postal_code,
+      :address,
+      :name
+    )
+  end
 end
