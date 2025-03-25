53bf9b191980a80593d55401521cb290bf67c682
tom-kwkm499 <kwkm.tomoo@gmail.com>
2025-03-25 03:22:00 +0000

    insertErrorMessage/pub/item/show

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -27,23 +27,25 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    cart_item = CartItem.new(cart_item_params)
-    cart_item.customer_id = current_customer.id
-    cart_item.item_id = cart_item_params[:item_id]
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.item_id = cart_item_params[:item_id]
     if check_cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: current_customer.id)
       check_cart_item.amount += params[:cart_item][:amount].to_i
-      check_cart_item.update(amount: cart_item.amount)
+      check_cart_item.update(amount: @cart_item.amount)
       flash[:notice] = "すでにカートに存在している商品です"
       redirect_to cart_items_path
     else
-      cart_item = CartItem.new(cart_item_params)
-      cart_item.customer_id = current_customer.id
-      if cart_item.save
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      if @cart_item.save
         flash[:notice] = "カートに追加しました"
         redirect_to cart_items_path
       else
         flash[:notice] = "個数を選択してください"
-        redirect_back(fallback_location: root_path)
+        @item = Item.find(params[:cart_item][:item_id])
+
+        render "public/items/show"
       end
     end
   end
